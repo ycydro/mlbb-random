32773dff4631e9f4390e25927309fa292d847a75
ycydro <180024097+ycydro@users.noreply.github.com>
2026-04-03 23:26:29 +0800

    chore: add shadcn components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
     >
       <body className="min-h-screen flex flex-col justify-center">
         {children}
+        <Toaster richColors />
       </body>
     </html>
   );
